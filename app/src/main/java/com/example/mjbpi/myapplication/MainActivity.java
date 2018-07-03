0559b366711c775566916edf79994f9c701a20e3
scaredn00b <Philipp1.Hartl@stud.uni-regensburg.de>
2018-07-03 15:05:49 +0200

    Added a string and calced the number 10.

--- app/src/main/java/com/example/mjbpi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mjbpi/myapplication/MainActivity.java
@@ -7,9 +7,13 @@ public class MainActivity extends AppCompatActivity {
 
     // intitial state
 
+    private String test = "test";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int fünf = 5;
+        int zehn = fünf * 2;
     }
 }
