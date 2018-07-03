f8cec4b1bc11165018743ff203b16008fef8ed19
pichlmichl <mjbpichlmeier8@gmail.com>
2018-07-03 14:44:49 +0200

    git

---
+++ app/src/main/java/com/example/mjbpi/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mjbpi.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // intitial state
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
